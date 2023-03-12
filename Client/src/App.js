b38d8ecbe37da1b04d59a8026425b292a002f875
Asif Muhammed <asifasifpsps@gmail.com>
2023-03-12 17:37:40 +0530

    Feat:Essential packages installed

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,7 +1,10 @@
+import { Route } from "react-router-dom";
+import Homepage from "./Pages/Homepage";
 
 function App() {
   return (
     <div className="App">
+      <Route path="/" exact component={Homepage} />
 
     </div>
   );
