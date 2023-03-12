47b3cd5b9cc1af73c33b9e0403e807f17c5eb483
Asif Muhammed <asifasifpsps@gmail.com>
2023-03-12 21:13:33 +0530

    Transporter page under construction

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,10 +1,12 @@
 import { Route } from "react-router-dom";
 import {Login} from "./Pages/Signin/Login";
+import {Transporter} from "./Pages/Transporter";
 
 function App() {
   return (
     <div className="App">
       <Route path="/" exact component={Login} />
+      <Route path="/transporter" exact component={Transporter} />
 
     </div>
   );
