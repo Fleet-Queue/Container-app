a984d36cb8ce175f0ea0445d3c1689c9d18ce3d0
mohammed yaseen <mhdyaseen.official@gmail.com>
2023-03-13 20:27:43 +0530

    update:side drawer added

--- Client/src/Pages/Transporter.js
+++ Client/src/Pages/Transporter.js
@@ -5,7 +5,7 @@ import {
   Avatar,
   HStack,
   Link,
-  IconButton,
+
   Button,
   Menu,
   MenuButton,
@@ -17,24 +17,14 @@ import {
   Stack,
   Center,
 } from '@chakra-ui/react';
-import Slider from 'react-slick';
 
-import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+
+import {SideDrawer} from '../components/miscellaneous/sideDrawer';
+
+
 const Links = ['Dashboard', 'Projects', 'Team'];
 
-const NavLink = ({ children }: { children: ReactNode }) => (
-    <Link
-      px={2}
-      py={1}
-      rounded={'md'}
-      _hover={{
-        textDecoration: 'none',
-        bg: useColorModeValue('gray.200', 'gray.700'),
-      }}
-      href={'#'}>
-      {children}
-    </Link>
-  );
+
   
 
 export  const Transporter = () => {
@@ -42,25 +32,21 @@ export  const Transporter = () => {
 
   return (
     <>
+    
       <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
         <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-          <IconButton
-            size={'md'}
-            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-            aria-label={'Open Menu'}
-            display={{ md: 'none' }}
-            onClick={isOpen ? onClose : onOpen}
-          />
+        <SideDrawer/>
+        
           <HStack spacing={8} alignItems={'center'}>
-            <Box>Logo</Box>
-            <HStack
+            <Box>Transporter</Box>
+            {/* <HStack
               as={'nav'}
               spacing={4}
               display={{ base: 'none', md: 'flex' }}>
               {Links.map((link) => (
                 <NavLink key={link}>{link}</NavLink>
               ))}
-            </HStack>
+            </HStack> */}
           </HStack>
           <Flex alignItems={'center'}>
             <Menu>
@@ -87,7 +73,7 @@ export  const Transporter = () => {
           </Flex>
         </Flex>
 
-        {isOpen ? (
+        {/* {isOpen ? (
           <Box pb={4} display={{ md: 'none' }}>
             <Stack as={'nav'} spacing={4}>
               {Links.map((link) => (
@@ -95,17 +81,10 @@ export  const Transporter = () => {
               ))}
             </Stack>
           </Box>
-        ) : null}
+        ) : null} */}
       </Box>
 
-      <Box p={4}>
-        <Center bg='tomato' h='200px' color='white'>
-  This is the Center
-</Center>
-<Center bg='tomato' h='200px' color='white'>
-  This is the Center
-</Center>
-</Box>
+      
     </>
   );
 }
---
+++ Client/src/components/miscellaneous/sideDrawer.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Box, Tooltip, Button, Text, Menu,  IconButton, MenuButton, MenuList, MenuItem, MenuDivider,Drawer, useDisclosure,Image, DrawerContent,DrawerBody,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerCloseButton,Input, useToast } from "@chakra-ui/react";
+    import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+export function SideDrawer(){
+ 
+   
+        const { isOpen, onOpen, onClose } = useDisclosure()
+        const btnRef = React.useRef()
+      
+        return (
+          <>
+            <IconButton
+            size={'md'}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={'Open Menu'}
+            ref={btnRef}
+            onClick={onOpen}
+          
+          />
+            
+            <Drawer
+              isOpen={isOpen}
+              placement='right'
+              onClose={onClose}
+              finalFocusRef={btnRef}
+            >
+              <DrawerOverlay />
+              <DrawerContent>
+                <DrawerCloseButton />
+                <DrawerHeader>Create your account</DrawerHeader>
+      
+                <DrawerBody>
+                  <Input placeholder='Type here...' />
+                </DrawerBody>
+      
+                <DrawerFooter>
+                  <Button variant='outline' mr={3} onClick={onClose}>
+                    Cancel
+                  </Button>
+                  <Button colorScheme='blue'>Save</Button>
+                </DrawerFooter>
+              </DrawerContent>
+            </Drawer>
+          </>
+        )
+      
+  
+}
+
