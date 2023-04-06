159372ac70a8642eb6a23e3c2c0d459e3e6280ff
Asif Muhammed <asifasifpsps@gmail.com>
2023-04-06 14:13:22 +0530

    User-mode selection page completed but some modification required

--- Client/src/Pages/UserModeSelection.js
+++ Client/src/Pages/UserModeSelection.js
@@ -1,61 +1,53 @@
-import React from 'react';
-import { Box, Image, Heading, Flex, useDisclosure, Button, Grid, GridItem ,Text } from '@chakra-ui/react';
+import { Box, Image, Heading, Flex, useDisclosure, Button, Grid, GridItem, Text } from '@chakra-ui/react';
 import forwarder from '../icons/forwarder-100x100.png';
 import transporter from '../icons/truck-100x100.png';
-import {
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
-} from '@chakra-ui/react'
-
-
-  
+import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
-
-
 
 const UserModeSelection = (details) => {
   console.log(details);
 
-const onClose = () => {
-  details.close();
-}
+  const onClose = () => {
+    details.close();
+  };
+
   return (
-
-
-  
-    <Modal isOpen={details.open} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>          
-
-        
-<Grid templateColumns='repeat(5, 1fr)' gap={4}>
-  <GridItem colSpan={2} h='10' bg='tomato'>
-    <Text>Text in Grid Item 1</Text>
-  </GridItem>
-  <GridItem colStart={4} colEnd={6} h='10' bg='papayawhip'>
-    <Text>Text in Grid Item 2</Text>
-  </GridItem>
-</Grid>
-
-          </ModalBody>
-
-          <ModalFooter>
-            <Button colorScheme='blue' mr={3} onClick={onClose}>
-              Close
-            </Button>
-            <Button variant='ghost'>Secondary Action</Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-
+    <Modal isOpen={details.open} onClose={onClose} size="xs">
+      <ModalOverlay />
+      <ModalContent minHeight="300px" maxHeight="600px">
+        <ModalHeader>Choose it</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Grid templateColumns="repeat(5, 1fr)" gap={2}>
+            <GridItem colSpan={2} display="flex" alignItems="center">
+              <Flex align="center" justify="center" direction="column">
+                <Text fontSize="xl" fontWeight="bold">
+                  Transporter
+                </Text>
+                <Box w="80px" h="80px" bg="gray.100" borderRadius="full" mt={2}>
+                  <Image src={transporter} alt="Transporter" w="60px" h="60px" mx="auto" my={3} />
+                </Box>
+              </Flex>
+            </GridItem>
+            <GridItem colStart={4} colEnd={6} display="flex" alignItems="center">
+            <Flex align="center" justify="center" direction="column">
+                <Text fontSize="xl" fontWeight="bold">
+                  Forwarder
+                </Text>
+                <Box w="80px" h="80px" bg="gray.100" borderRadius="full" mt={2}>
+                  <Image src={forwarder} alt="Forwarder" w="60px" h="60px" mx="auto" my={3} />
+                </Box>
+              </Flex>
+            </GridItem>
+          </Grid>
+        </ModalBody>
+        <ModalFooter>
+          <Button colorScheme="blue" mr={3} onClick={onClose}>
+            Close
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
   );
 };
 
